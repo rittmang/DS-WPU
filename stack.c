06f659b794db01a742facb8d6c561a6bd2a80ae3
rittmang <rightonrittman@gmail.com>
2018-12-19 08:45:28 +0530

    Add some functions

--- stack.c
+++ stack.c
@@ -1,1 +1,21 @@
+#define MAX 30;
 #include<stdio.h>
+
+typedef struct{
+      char pile[MAX];
+      int top;
+  }stack;
+
+int  isFull(stack s)
+{
+  if(s.top==MAX-1)
+    return 1;
+  else return 0;
+}
+int isEmpty(stack s)
+{
+  if(s.top==-1)
+    return 1;
+  else return 0;
+}
+void push(stack s,)
